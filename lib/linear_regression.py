00d447b41eb8496251dbc7e2be61efcb5b4919aa
Alfredo Motta <amotta@housetrip.com>
2015-12-24 17:54:02 +0000

    refactor x with ones creation

--- lib/linear_regression.py
+++ lib/linear_regression.py
@@ -15,29 +15,43 @@ class BatchGradient(LinearRegression):
         self.iterations = settings['iterations']
 
     def predict(self, X):
-        return np.dot(self.x, self.theta)
+        x = self._add_ones_to(X)
+        return np.dot(x, self.theta)
 
     def fit(self, X, y):
         n, dim = X.shape
 
         self.theta = np.zeros(dim + 1)
-        self.x = np.ones([n, dim+1])
-        self.x[: , 1: ] = X
+        x = self._add_ones_to(X)
 
         for i in range(self.iterations):
             self.theta = self.theta - \
                          (self.alpha / n) * \
-                         (self.x.dot(self.theta) - y).T.dot(self.x)
+                         (x.dot(self.theta) - y).T.dot(x)
+
+    def _add_ones_to(self, X):
+        n, dim = X.shape
+        x = np.ones([n, dim+1])
+        x[: , 1: ] = X
+
+        return x
+
 
 
 class LeastSquares(LinearRegression):
     def predict(self, X):
-        return self.x.dot(self.beta)
+        x = self._add_ones_to(X)
+
+        return x.dot(self.beta)
 
     def fit(self, X, y):
+        x = self._add_ones_to(X)
+
+        self.beta = np.linalg.inv(np.dot(x.T, x)).dot(x.T).dot(y)
+
+    def _add_ones_to(self, X):
         n, dim = X.shape
-        self.x = np.ones([n, dim+1])
-        self.x[: , 1: ] = X
+        x = np.ones([n, dim+1])
+        x[: , 1: ] = X
 
-        self.beta = np.linalg.inv(np.dot(self.x.T, self.x)).dot(self.x.T).dot(y)
-
+        return x
